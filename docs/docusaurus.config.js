2229f45c429da44917866502e6e79d4b47af8582
Finbar <finbar@finbar.xyz>
2025-05-25 13:58:52 -0400

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,6 +19,8 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -88,7 +90,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/getting-started',
               },
             ],
           },
