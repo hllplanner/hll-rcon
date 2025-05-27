d424b3377c4c087fab1855ab43cd4f3ef94c2396
Finbar <finbar@finbar.xyz>
2025-05-26 21:39:08 -0400

    update docs

---
+++ docs/docs/IRCONClient.md
@@ -0,0 +1,27 @@
+---
+sidebar_position: 2
+title: IRCONClient
+description: The IRCONClient
+---
+
+The `IRCONClient` is the primary client, exposing methods from both the `RCONClientV1` and `RCONClientV2` classes. When
+executing a command, it will always favor the RCONv2 counterpart due to the protocol's enhanced speed and
+reliability. If the command is not available for RCONv2, it will favor the RCONv1 counterpart, and vice versa.
+
+### Constructor
+```js
+const { IRCONClient } = require("hll-ircon");
+
+const client = new IRCONClient({
+  host: "127.0.0.1",            // Server Host
+  port: 8000,                   // RCON Port
+  password: "RCON_PASSWORD",    // RCON Password
+  
+  v1InactivityTimeout: 1_000,   // Timeout to wait for the last message in RCONv1
+  
+  enableLogPolling: true,       // Enable log polling to enable "newLog" event
+  pollSource: 2,                // From which RCON client version instance to poll logs from
+  pollWindow: 30,               // How far to backtrack logs in seconds
+  pollFrequency: 1              // How frequently to poll new logs in milliseconds
+});
+```
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,121 +4,117 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: 'hll-ircon',
-  tagline: 'Integrated RCON Client for HLL RCONv1 and RCONv2',
-  favicon: 'img/ircon-favicon.ico',
+  title: "hll-ircon",
+  tagline: "Integrated RCON Client for HLL RCONv1 and RCONv2",
+  favicon: "img/ircon-favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://ircon.hllplanner.net',
+  url: "https://ircon.hllplanner.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
   deploymentBranch: "gh-pages",
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'hllplanner', // Usually your GitHub org/user name.
-  projectName: 'hll-ircon', // Usually your repo name.
+  organizationName: "hllplanner", // Usually your GitHub org/user name.
+  projectName: "hll-ircon", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"]
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: "./sidebars.js",
+          editUrl: "https://github.com/hllplanner/hll-ircon/tree/master/docs"
         },
         theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
+          customCss: "./src/css/custom.css"
+        }
+      })
+    ]
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/ircon-primary-logo.png',
+      image: "img/ircon-primary-logo.png",
       navbar: {
-        title: 'hll-ircon',
         logo: {
-          alt: '',
-          src: 'img/ircon-favicon.png',
+          alt: "HLL-IRCON",
+          src: "img/ircon-primary-logo-transparent.png"
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentation',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentation"
           },
           {
-            href: 'https://github.com/hllplanner/hll-ircon',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            href: "https://github.com/hllplanner/hll-ircon",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/getting-started',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/D3HaSRvUaJ',
+                label: "Tutorial",
+                to: "/docs/getting-started"
               }
-            ],
+            ]
           },
           {
-            title: 'More',
+            title: "Community",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/hllplanner/hll-ircon',
-              },
-            ],
+                label: "Discord",
+                href: "https://discord.gg/D3HaSRvUaJ"
+              }
+            ]
           },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/hllplanner/hll-ircon"
+              }
+            ]
+          }
         ]
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+        darkTheme: prismThemes.dracula
+      }
+    })
 };
 
 export default config;
